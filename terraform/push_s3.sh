1eacc4116a09e259b8ed6cb6e1795dfcf5f5ba40
hoangviet148 <nnvvhh148@gmail.com>
2022-12-06 10:29:41 +0700

    update

--- terraform/push_s3.sh
+++ terraform/push_s3.sh
@@ -1,4 +1,4 @@
-ec2_instances=(jenkins vpn-jump)
+ec2_instances=(cicd data_viz elasticsearch etl jenkins_slave kafka kong kong_cluster neo4j)
 FOLDER="/home/hoangnv46/Documents/terraform_ng/prod/ap-southeast-1/prod/ec2"
 ROLE="arn:aws:iam::921042051488:role/prod-terraform"
 folder_to_backup="/home/hoangnv46/Documents/script/terraform/state"
@@ -6,12 +6,16 @@ folder_to_backup="/home/hoangnv46/Documents/script/terraform/state"
 for instance in ${ec2_instances[@]}
 do 
     cd $FOLDER && mkdir -p ${instance} && \
-    cp terragrunt.hcl ${instance}
+    cp terragrunt.hcl ${instance} && \
+    mv ${instance}.tf ./${instance}
 done
 
 rm $FOLDER/terragrunt.hcl
 rm $FOLDER/provider.tf
 rm $FOLDER/backend.tf
+
+rm $FOLDER/.terraform.lock
+rm -rf $FOLDER/.terraform
 
 for instance in ${ec2_instances[@]}
 do 
