71c1e845890790591b2f38e532918e24ee775d91
hoangviet148 <nnvvhh148@gmail.com>
2022-12-08 11:02:27 +0700

    update

--- terraform/backup.sh
+++ terraform/backup.sh
@@ -1,6 +1,6 @@
-FOLDER="/home/hoangnv46/Documents/terraform_ng/prod/ap-southeast-1/prod/ec2"
-folder_to_backup="/home/hoangnv46/Documents/script/terraform/state"
+FOLDER="/home/hoangnv46/Documents/terraform_ng/prod/ap-southeast-1/prod/ec2/kong_cluster"
+file_to_backup="/home/hoangnv46/Documents/script/terraform/state/kong_cluster_old.tfstate"
 ROLE="arn:aws:iam::921042051488:role/prod-terraform"
 
 cd $FOLDER && \
-terragrunt state pull > $folder_to_backup/ec2.tfstate --terragrunt-iam-role $ROLE
+terragrunt state pull > $file_to_backup --terragrunt-iam-role $ROLE
--- terraform/temp.sh
+++ terraform/temp.sh
@@ -1,31 +1,34 @@
+DIR="/home/hoangnv46/Documents/terraform_ng/prod/ap-southeast-1/prod/ec2/kong_cluster"
+ROLE="arn:aws:iam::921042051488:role/prod-terraform"
+folder_to_backup="/home/hoangnv46/Documents/script/terraform/state"
+
+###################################################################
+
 declare -A ec2_instances_1
-ec2_instances_1=(['cicd']='ec2' 
-                 ['data_viz']='ec2_data_viz'
-                 ['elasticsearch']='ec2_es["one"]'
-                 ['etl']='ec2_etl'
-                 ['jenkins_slave']='jenkins_slave'
-                 ['kafka']='ec2_kafka["one"]'
-                 ['kong']='ec2_kong'
-                 ['kong_cluster']='ec2_kong_control_node'
-                 ['tagging2_mongodb']='ec2_tagging2'
-                 ['tagging_mongodb']='ec2_mongodb')
+ec2_instances_1=(['kong_cluster']='ec2_kong_control_node')
 
 declare -A ec2_instances_2
-ec2_instances_2=(['elasticsearch']='ec2_es["two"]'
-                 ['kafka']='ec2_kafka["two"]'
-                 ['kong_cluster']='ec2_kong_data_node')
+ec2_instances_2=(['kong_cluster']='ec2_kong_data_node')
 
-declare -A ec2_instances_3
-ec2_instances_3=(['elasticsearch']='ec2_es["three"]'
-                 ['kafka']='ec2_kafka["three"]')
+for instance in ${!ec2_instances_1[@]}
+do
+    cd $DIR && \
+    terragrunt state mv -state=$folder_to_backup/kong_cluster_old.tfstate -state-out=$folder_to_backup/${instance}.tfstate module.${ec2_instances_1[${instance}]} module.${ec2_instances_1[${instance}]} --terragrunt-iam-role $ROLE 
+done
 
-declare -A arrays_ec2=( (['test_1']=1) (['test_2']=2) )
-echo ${arrays_ec2[0,0]}
-for array in "${arrays_ec2[@]}"
-do  
-    echo "${array[*]}"
-    for item in ${!array[@]}
-    do
-        echo test
-    done
+for instance in ${!ec2_instances_2[@]}
+do
+    cd $DIR && \
+    terragrunt state mv -state=$folder_to_backup/kong_cluster_old.tfstate -state-out=$folder_to_backup/${instance}.tfstate module.${ec2_instances_2[${instance}]} module.${ec2_instances_2[${instance}]} --terragrunt-iam-role $ROLE 
+done
+
+###########################################################
+
+declare -A ec2_sg
+ec2_sg=(['kong_cluster']='kong_cluster_security_group')
+
+for sg in ${!ec2_sg[@]}
+do
+    cd $DIR && \
+    terragrunt state mv -state=$folder_to_backup/kong_cluster_old.tfstate -state-out=$folder_to_backup/${sg}.tfstate module.${ec2_sg[${sg}]} module.ec2_kong_cluster_security_group --terragrunt-iam-role $ROLE
 done
